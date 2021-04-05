0c44299109b05f656c127a8ee254f278c1f2e1e2
vinicius <vnsqzs@gmail.com>
2021-04-05 05:20:07 -0400

    about page scripts

--- js/ball.js
+++ js/ball.js
@@ -1,42 +1,50 @@
-let xStartBall
-let yStartBall
-let xSpeed
-let ySpeed
-let canvas
+let xStartBall;
+let yStartBall;
+let xSpeed;
+let ySpeed;
+let canvas;
+let ballX, ballY;
 
-function setup() {
-    canvas = createCanvas(windowWidth, windowHeight)
-    canvas.position(0, 0, 'fixed')
-    canvas.style('z-index', '-20')
-    xStartBall = random(1920)
-    yStartBall = random(1897)
-    xSpeed = 10;
-    ySpeed = 10;
+if (windowWidth <= 768) {
+  ballX = 50;
+  ballY = 50;
+} else {
+  ballX = 200;
+  ballY = 200;
 }
 
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0, "fixed");
+  canvas.style("z-index", "-20");
+  xStartBall = random(1920);
+  yStartBall = random(1897);
+  xSpeed = 10;
+  ySpeed = 10;
+}
 
 function draw() {
-    clear()
-    ellipse(xStartBall, yStartBall, 200, 200)
-    noStroke()
-    fill(0,0,255)
+  clear();
+  ellipse(xStartBall, yStartBall, ballX, ballY);
+  noStroke();
+  fill(0, 0, 255);
 
-    xStartBall = xStartBall + xSpeed
-    yStartBall = yStartBall + ySpeed
+  xStartBall = xStartBall + xSpeed;
+  yStartBall = yStartBall + ySpeed;
 
-    if (xStartBall + 30 >= windowWidth) {
-        xSpeed = -xSpeed
-        xStartBall = windowWidth - 30
-    } else if (xStartBall <= 0) {
-        xSpeed = -xSpeed
-        xStartBall = 0
-    }
+  if (xStartBall + 30 >= windowWidth) {
+    xSpeed = -xSpeed;
+    xStartBall = windowWidth - 30;
+  } else if (xStartBall <= 0) {
+    xSpeed = -xSpeed;
+    xStartBall = 0;
+  }
 
-    if (yStartBall + 30 >= windowHeight) {
-        ySpeed = -ySpeed
-        yStartBall = windowHeight - 30
-    } else if (yStartBall <= 0) {
-        ySpeed = -ySpeed
-        yStartBall = 0
-    }  
+  if (yStartBall + 30 >= windowHeight) {
+    ySpeed = -ySpeed;
+    yStartBall = windowHeight - 30;
+  } else if (yStartBall <= 0) {
+    ySpeed = -ySpeed;
+    yStartBall = 0;
+  }
 }
